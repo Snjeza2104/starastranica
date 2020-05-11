4ede0b1d23f72c9eaaf6d36cdf0d0512b6a03c2c
Snježa <snjeza.mijosevic@gmail.com>
2020-05-11 10:31:19 +0200

    Stara web stranica

---
+++ web stranica iz studentskih dana/dan_datum_sat.js
@@ -0,0 +1,39 @@
+﻿function startTime()
+{
+var today=new Date();
+var h=today.getHours();
+var m=today.getMinutes();
+var s=today.getSeconds();
+var day=today.getDate();
+var month=today.getMonth()+1;
+var year=today.getFullYear();
+var dan=today.getDay();
+var ime_dana;
+// add a zero in front of numbers<10
+m=checkTime(m);
+s=checkTime(s);
+if (dan==0) ime_dana="nedjelja";
+else if (dan==1) 
+ime_dana="ponedjeljak";
+else if (dan==2) 
+ime_dana="utorak";
+else if (dan==3) 
+ime_dana="srijeda";
+else if (dan==4) 
+ime_dana="četvrtak";
+else if (dan==5) 
+ime_dana="petak";
+else 
+ime_dana="subota";
+document.getElementById('txt').innerHTML=ime_dana+", "+day+"."+month+"."+year+".  "+h+":"+m+":"+s
+t=setTimeout('startTime()',500);
+}
+
+function checkTime(i)
+{
+if (i<10)
+  {
+  i="0" + i;
+  }
+return i;
+}
---
+++ web stranica iz studentskih dana/datum_zadnje_promjene.js
@@ -0,0 +1,3 @@
+var date = new Date(document.lastModified); 
+document.write("Datum zadnje promjene: "+date.getDate()+ "." + (date.getMonth()+1)+ "." + date.getFullYear()+"."); 
+		
